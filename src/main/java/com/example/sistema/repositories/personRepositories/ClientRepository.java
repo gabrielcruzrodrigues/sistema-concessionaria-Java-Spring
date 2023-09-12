e5c062ae68f30483e5a1b1c17be1f3b235bc34cc
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-09-12 20:39:07 -0300

    refactor ClientRepository

--- src/main/java/com/example/sistema/repositories/personRepositories/ClientRepository.java
+++ src/main/java/com/example/sistema/repositories/personRepositories/ClientRepository.java
@@ -2,8 +2,16 @@ package com.example.sistema.repositories.personRepositories;
 
 import com.example.sistema.models.personModels.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    @Query(value = "SELECT * FROM tb_clients WHERE cpf = :cpf", nativeQuery = true)
+    Optional<Client> findByClientCpf(@Param("cpf") String cpf);
 }
