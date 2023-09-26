4364f10b4571c2456f9012897e6fac2711c9fe11
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-09-26 17:08:18 -0300

    creating initial data load

--- src/main/java/com/example/sistema/models/Car.java
+++ src/main/java/com/example/sistema/models/Car.java
@@ -10,7 +10,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Entity
 @Table(name = "TB_CARS")
 @Data
@@ -19,34 +21,49 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode
 public class Car extends Vehicle {
 
+
     public interface CreateCar{}
     public interface UpdateCar{}
 
-    @Column(name = "electricWindows", nullable = false)
+    @Column(name = "electricWindows")
     @NotNull(groups = CreateCar.class)
     private Boolean electricWindows;
 
-    @Column(name = "automaticTransmission", nullable = false)
+    @Column(name = "automaticTransmission")
     @NotNull(groups = CreateCar.class)
     private Boolean automaticTransmission;
 
-    @Column(name = "traction", nullable = false)
+    @Column(name = "traction")
     @NotNull(groups = CreateCar.class)
-    private Boolean traction;
+    private String traction;
 
-    @Column(name = "hydraulicSteering", nullable = false)
+    @Column(name = "hydraulicSteering")
     @NotNull(groups = CreateCar.class)
     private Boolean hydraulicSteering;
 
-    @Column(name = "gps", nullable = false)
+    @Column(name = "gps")
     @NotNull(groups = CreateCar.class)
     private Boolean gps;
 
-    @Column(name = "sunroof", nullable = false)
+    @Column(name = "sunroof")
     @NotNull(groups = CreateCar.class)
     private Boolean sunroof;
 
-    @Column(name = "sensorsAndCameras", nullable = false)
+    @Column(name = "sensorsAndCameras")
     @NotNull(groups = CreateCar.class)
     private Boolean sensorsAndCameras;
+    public Car(String name, String plate, String color, String chassi, String model, String stage, Double mileage, String weight, String fuelConsumptionPerLiter, String motor, String power, String torque,
+               boolean isAvailable, boolean electricWindows, boolean automaticTransmission, String traction, boolean hydraulicSteering, boolean gps, boolean sunroof, boolean sensorsAndCameras) {
+
+        super(name, plate, color, chassi, model, stage, mileage, weight, fuelConsumptionPerLiter, motor, power, torque, isAvailable);
+        this.electricWindows = electricWindows;
+        this.automaticTransmission = automaticTransmission;
+        this.traction = traction;
+        this.hydraulicSteering = hydraulicSteering;
+        this.gps = gps;
+        this.sunroof = sunroof;
+        this.sensorsAndCameras = sensorsAndCameras;
+
+        log.info("passou construtor");
+    }
 }
--- src/main/java/com/example/sistema/models/superClass/Vehicle.java
+++ src/main/java/com/example/sistema/models/superClass/Vehicle.java
@@ -18,36 +18,34 @@ import java.util.Date;
 @EqualsAndHashCode
 public class Vehicle {
 
-    public interface CreateVehicle{}
-    public interface UpdateVehicle{}
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
     private Long id;
 
-    @Column(name = "name", length = 30, nullable = false)
-    @NotNull(groups = CreateVehicle.class)
-    @NotBlank(groups = CreateVehicle.class)
-    @Size(groups = CreateVehicle.class, min = 3, max = 30)
+    @Column(name = "name", length = 30)
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 30)
     private String name;
 
-    @Column(name = "plate", length = 7, nullable = false, unique = true)
-    @NotNull(groups = CreateVehicle.class)
-    @NotBlank(groups = CreateVehicle.class)
-    @Size(groups = CreateVehicle.class, min = 7, max = 7)
+    @Column(name = "plate", length = 7)
+    @NotNull
+    @NotBlank
+    @Size(min = 7, max = 7)
     private String plate;
 
-    @Column(name = "color", length = 20, nullable = false)
-    @NotNull(groups = CreateVehicle.class)
-    @NotBlank(groups = CreateVehicle.class)
-    @Size(groups = CreateVehicle.class, min = 3, max = 20)
+    @Column(name = "color", length = 20)
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 20)
     private String color;
 
-    @Column(name = "chassi", length = 17, nullable = false, unique = true)
-    @NotNull(groups = CreateVehicle.class)
-    @NotBlank(groups = CreateVehicle.class)
-    @Size(groups = CreateVehicle.class, min = 3, max = 17)
+    @Column(name = "chassi", length = 17)
+    @NotNull
+    @NotBlank
+    @Size(min = 17, max = 17)
     private String chassi;
 
 //    @Column(name = "manufacturingDate", nullable = false)
@@ -55,49 +53,65 @@ public class Vehicle {
 //    @NotBlank(groups = CreateVehicle.class)
 //    private Date manufacturingDate;
 
-    @Column(name = "model", length = 30, nullable = false)
-    @NotNull(groups = CreateVehicle.class)
-    @NotBlank(groups = CreateVehicle.class)
-    @Size(groups = CreateVehicle.class, min = 3, max = 30)
+    @Column(name = "model", length = 30)
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 30)
     private String model;
 
-    @Column(name = "stage", length = 5, nullable = false)
-    @NotNull(groups = CreateVehicle.class)
-    @NotBlank(groups = CreateVehicle.class)
-    @Size(groups = CreateVehicle.class, min = 4, max = 5)
+    @Column(name = "stage", length = 5)
+    @NotNull
+    @NotBlank
+    @Size(min = 4, max = 5)
     private String stage;
 
-    @Column(name = "mileage", nullable = false)
-    @NotNull(groups = CreateVehicle.class)
-    @NotBlank(groups = CreateVehicle.class)
+    @Column(name = "mileage")
+    @NotNull
     private Double mileage;
 
-    @Column(name = "weight", nullable = false)
-    @NotNull(groups = CreateVehicle.class)
-    @NotBlank(groups = CreateVehicle.class)
-    private Double weight;
+    @Column(name = "weight")
+    @NotNull
+    @NotBlank
+    private String weight;
 
-    @Column(name = "fuelConsumptionPerLiter", nullable = false)
-    @NotNull(groups = CreateVehicle.class)
-    @NotBlank(groups = CreateVehicle.class)
-    private Double fuelConsumptionPerLiter;
+    @Column(name = "fuelConsumptionPerLiter")
+    @NotNull
+    @NotBlank
+    private String fuelConsumptionPerLiter;
 
-    @Column(name = "motor", length = 10, nullable = false)
-    @NotNull(groups = CreateVehicle.class)
-    @NotBlank(groups = CreateVehicle.class)
-    @Size(groups = CreateVehicle.class, min = 5, max = 10)
+    @Column(name = "motor", length = 10)
+    @NotNull
+    @NotBlank
+    @Size(min = 5, max = 10)
     private String motor;
 
-    @Column(name = "power", nullable = false)
-    @NotNull(groups = CreateVehicle.class)
-    @NotBlank(groups = CreateVehicle.class)
-    private Double power;
+    @Column(name = "power")
+    @NotNull
+    @NotBlank
+    private String power;
 
-    @Column(name = "torque", nullable = false)
-    @NotNull(groups = CreateVehicle.class)
-    @NotBlank(groups = CreateVehicle.class)
-    private Double torque;
+    @Column(name = "torque")
+    @NotNull
+    @NotBlank
+    private String torque;
 
-    @Column(name = "isAvailable", nullable = false)
+    @Column(name = "isAvailable")
     private Boolean isAvailable;
+    public Vehicle(String name, String plate, String color, String chassi, String model, String stage, Double mileage,
+                   String weight, String fuelConsumptionPerLiter, String motor, String power, String torque, boolean isAvailable) {
+
+        this.name = name;
+        this.plate = plate;
+        this.color = color;
+        this.chassi = chassi;
+        this.model = model;
+        this.stage = stage;
+        this.mileage = mileage;
+        this.weight = weight;
+        this.fuelConsumptionPerLiter = fuelConsumptionPerLiter;
+        this.motor = motor;
+        this.power = power;
+        this.torque = torque;
+        this.isAvailable = isAvailable;
+    }
 }
